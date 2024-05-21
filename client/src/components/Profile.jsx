9d09c469190bc9079f47d10cfd1df52d1e67de42
KesharwaniShivam <shivamkesharwani6767@gmail.com>
2024-05-21 18:51:42 +0530

    checking in profile

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,10 +1,15 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { Context } from '../main'
 import Loader from './Loader'
 
 function Profile() {
 
   const{user, isAuthenticated, loading} = useContext(Context)
+
+  useEffect(() => {
+   
+  }, [isAuthenticated]);
+  
 
   // console.log(user)
 
